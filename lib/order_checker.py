8c4651bb4d83db2938f6bc7c54e1a9ec4c4899de
Danil Semelenov <mail@danil.mobi>
2017-10-11 08:56:35 +0300

    Take into account only lambda assignments, rename propperties

--- lib/order_checker.py
+++ lib/order_checker.py
@@ -4,33 +4,33 @@ import pylint.interfaces
 
 class OrderChecker(pylint.checkers.BaseChecker):
   __implements__ = (pylint.interfaces.IAstroidChecker,)
-  decorator = None
-  function = None
   name = 'order-checker'
-  variable = None
+  previous_decorator = None
+  previous_function = None
+  previous_lambda = None
 
   msgs = {
     'W0001': ("Decorator %s is not defined in sorted order", 'unordered-decorator', ''),
     'W0002': ("Function %s is not defined in sorted order", 'unordered-function', ''),
-    'W0003': ("Variable %s is not defined in sorted order", 'unordered-variable', ''),
+    'W0003': ("Lambda %s is not defined in sorted order", 'unordered-lambda', ''),
   }
 
   def visit_assignname(self, node):
-    if not node.col_offset:
-      if self.variable and self.variable > node.name:
-        self.add_message('unordered-variable', args=node.name, node=node)
-      self.variable = node.name
+    if not node.col_offset and isinstance(node.assign_type().value, astroid.Lambda):
+      if self.previous_lambda and self.previous_lambda > node.name:
+        self.add_message('unordered-lambda', args=node.name, node=node)
+      self.previous_lambda = node.name
 
   def visit_functiondef(self, node):
     if not node.col_offset:
       if len(node.body) == 2 and isinstance(node.body[0], astroid.FunctionDef) and isinstance(node.body[1], astroid.Return):
-        if self.decorator and self.decorator > node.name:
+        if self.previous_decorator and self.previous_decorator > node.name:
           self.add_message('unordered-decorator', args=node.name, node=node)
-        self.decorator = node.name
+        self.previous_decorator = node.name
       else:
-        if self.function and self.function > node.name:
+        if self.previous_function and self.previous_function > node.name:
           self.add_message('unordered-function', args=node.name, node=node)
-        self.function = node.name
+        self.previous_function = node.name
 
 def register(linter):
   linter.register_checker(OrderChecker(linter))
