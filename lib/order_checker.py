5a56352b2016e076b595663a1078bb18912b6b98
Danil Semelenov <mail@danil.mobi>
2017-10-25 17:16:55 +0300

    Check order of class definitions

--- lib/order_checker.py
+++ lib/order_checker.py
@@ -5,14 +5,16 @@ import pylint.interfaces
 class OrderChecker(pylint.checkers.BaseChecker):
   __implements__ = (pylint.interfaces.IAstroidChecker,)
   name = 'order-checker'
+  previous_class = None
   previous_decorator = None
   previous_function = None
   previous_lambda = None
 
   msgs = {
     'W0001': ("Decorator %s is not defined in sorted order", 'unordered-decorator', ''),
-    'W0002': ("Function %s is not defined in sorted order", 'unordered-function', ''),
-    'W0003': ("Lambda %s is not defined in sorted order", 'unordered-lambda', ''),
+    'W0002': ("Class %s is not defined in sorted order", 'unordered-class', ''),
+    'W0003': ("Function %s is not defined in sorted order", 'unordered-function', ''),
+    'W0004': ("Lambda %s is not defined in sorted order", 'unordered-lambda', ''),
   }
 
   def visit_assignname(self, node):
@@ -20,6 +22,12 @@ class OrderChecker(pylint.checkers.BaseChecker):
       if self.previous_lambda and self.previous_lambda > node.name:
         self.add_message('unordered-lambda', args=node.name, node=node)
       self.previous_lambda = node.name
+
+  def visit_classdef(self, node):
+    if not node.col_offset:
+      if self.previous_class and self.previous_class > node.name:
+        self.add_message('unordered-class', args=node.name, node=node)
+      self.previous_class = node.name
 
   def visit_functiondef(self, node):
     if not node.col_offset:
