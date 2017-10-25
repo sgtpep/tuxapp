364946d0fbd47359e6883cabf55a55b4db3a62b2
Danil Semelenov <mail@danil.mobi>
2017-10-25 18:09:47 +0300

    Detect misplaced definitions, reduce a number of messages

--- lib/order_checker.py
+++ lib/order_checker.py
@@ -11,34 +11,38 @@ class OrderChecker(pylint.checkers.BaseChecker):
   previous_lambda = None
 
   msgs = {
-    'W0001': ("Decorator %s is not defined in sorted order", 'unordered-decorator', ''),
-    'W0002': ("Class %s is not defined in sorted order", 'unordered-class', ''),
-    'W0003': ("Function %s is not defined in sorted order", 'unordered-function', ''),
-    'W0004': ("Lambda %s is not defined in sorted order", 'unordered-lambda', ''),
+    'W0001': ("%s %s is misplaced", 'misplaced-item', ''),
+    'W0002': ("%s %s is not defined in sorted order", 'unordered-item', ''),
   }
 
   def visit_assignname(self, node):
     if not node.col_offset and isinstance(node.assign_type().value, astroid.Lambda):
       if self.previous_lambda and self.previous_lambda > node.name:
-        self.add_message('unordered-lambda', args=node.name, node=node)
+        self.add_message('unordered-item', args=("Lambda", node.name), node=node)
       self.previous_lambda = node.name
 
   def visit_classdef(self, node):
     if not node.col_offset:
       if self.previous_class and self.previous_class > node.name:
-        self.add_message('unordered-class', args=node.name, node=node)
+        self.add_message('unordered-item', args=("Class", node.name), node=node)
       self.previous_class = node.name
+      if self.previous_function or self.previous_lambda:
+        self.add_message('misplaced-item', args=("Class", node.name), node=node)
 
   def visit_functiondef(self, node):
     if not node.col_offset:
       if len(node.body) >= 2 and isinstance(node.body[-2], astroid.FunctionDef) and isinstance(node.body[-1], astroid.Return):
         if self.previous_decorator and self.previous_decorator > node.name:
-          self.add_message('unordered-decorator', args=node.name, node=node)
+          self.add_message('unordered-item', args=("Decorator", node.name), node=node)
         self.previous_decorator = node.name
+        if self.previous_class or self.previous_function or self.previous_lambda:
+          self.add_message('misplaced-item', args=("Decorator", node.name), node=node)
       else:
         if self.previous_function and self.previous_function > node.name:
-          self.add_message('unordered-function', args=node.name, node=node)
+          self.add_message('unordered-item', args=("Function", node.name), node=node)
         self.previous_function = node.name
+        if self.previous_lambda:
+          self.add_message('misplaced-item', args=("Function", node.name), node=node)
 
 def register(linter):
   linter.register_checker(OrderChecker(linter))
