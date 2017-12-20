c0de0fdbd9b827bfdcabf7859b19c5183cc6f4c8
Danil Semelenov <mail@danil.mobi>
2017-12-20 23:50:28 +0300

    Add a group

--- lib/appfile.py
+++ lib/appfile.py
@@ -25,6 +25,7 @@ get_groups = lambda: \
     'cloud-storage',
     'code-editors',
     'content-management',
+    'cryptocurrency-wallets',
     'database-management',
     'ebook-utilities',
     'email-clients',
