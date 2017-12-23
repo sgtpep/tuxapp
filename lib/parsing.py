952c2e29216eebefedf31615b194c103a02b014e
Danil Semelenov <mail@danil.mobi>
2017-12-23 06:35:28 +0300

    Improve github screenshots parsing

--- lib/parsing.py
+++ lib/parsing.py
@@ -191,7 +191,7 @@ class GitHubScreenshotURLsParser(BaseParser):
       self.is_article = True
     elif tag == 'p' and self.is_article:
       self.paragraph_number += 1
-    elif tag == 'img' and attributes.get('src') and self.previous_tag == 'a' and self.previous_attributes.get('href') and (is_image_url(self.previous_attributes['href']) or is_image_url(attributes.get('data-canonical-src', '')) or self.previous_attributes['href'] == attributes['src']) and self.is_article and self.paragraph_number > 1:
+    elif tag == 'img' and attributes.get('src') and self.previous_tag == 'a' and self.previous_attributes.get('href') and (is_image_url(self.previous_attributes['href']) or self.previous_attributes['href'] == attributes['src'] or is_image_url(attributes.get('data-canonical-src', '')) and self.previous_attributes['href'] == attributes['src']) and self.is_article and self.paragraph_number > 1:
       self.add_result(attributes['src'])
 
 class IconURLParser(BaseParser):
