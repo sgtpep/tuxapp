e1b14bb4c73c22d38f5cb200a579e9daabf45436
Danil Semelenov <mail@danil.mobi>
2017-12-26 17:40:22 +0300

    Change a pattern

--- lib/testing.py
+++ lib/testing.py
@@ -234,7 +234,7 @@ is_app_library_ignored = lambda app, library: library in tuxapp.query_appfile(ap
 is_app_process_output_ignored = lambda app, distribution, output: \
   bool(re.search({
     'drl': r"Can't open SDL_Mixer!",
-    'gnome-pomodoro': r'Failed to execute child process "dbus-launch"|The name org\.freedesktop\.Notifications was not provided by any \.service files',
+    'gnome-pomodoro': r' file notifications-capability\.c:|The name org\.freedesktop\.Notifications was not provided by any \.service files',
   }.get(app, r'\0'), output))
 
 request_arch_container_url = lambda: get_arch_mirror_url('iso/latest/') + tuxapp.request_grep_url(get_arch_mirror_url('iso/latest/'), ('-Po', '-m', '1', r'(?<=")archlinux-bootstrap-[^"]+'))
